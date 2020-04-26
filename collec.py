d0fed499856f16d45daa88c9d23c66de1ac49a06
aashrith2021 <aashrithkan2@gmail.com>
2020-04-26 19:22:59 +0530

    collections

---
+++ collec.py
@@ -0,0 +1,14 @@
+from array import array
+coll=[]
+coll.append('aash')
+print(coll)
+dicry={}
+dicry['huh']='haha'
+dicry['hola']='delooo'
+people=[]
+people.append(dicry)
+people.append({
+    'guju':'kello','yuff':'lol'
+})
+print(people)
+print(dicry)
