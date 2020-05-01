125f6f7640e09c0de8dabca102912fefcc93a653
aashrith2021 <aashrithkan2@gmail.com>
2020-05-01 22:02:29 +0530

    strings

---
+++ strings1.py
@@ -0,0 +1,7 @@
+firstname='aashrith'
+lastname='kandimalla'
+print('hello '+firstname + ' '+lastname)
+position=input('position\n')
+print(position.capitalize()+position.upper() \
++str(position.count('a'))+position.lower())
+
